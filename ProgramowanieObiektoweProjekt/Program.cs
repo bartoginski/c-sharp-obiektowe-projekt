32b8d6b4dac1e3ec144a03eb6339397e923122b7
Jan Mrozewski <jan.mrozewski@gmail.com>
2025-04-15 18:15:39 +0200

    Dodanie klasy tworzącej planszę oraz reszta.Klasa do towrzenia planszy

--- ProgramowanieObiektoweProjekt/Program.cs
+++ ProgramowanieObiektoweProjekt/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ProgramowanieObiektoweProjekt;
+﻿using BoardNamespace;
 using Spectre.Console;
 
 class Program
 {
     static void Main(string[] args)
     {
-        AnsiConsole.Markup("[underline red]Battleships[/] Game!");
+        var board1 = new Board();
+        var board2 = new Board();
+
+        var tabela = new Table();
+
+        tabela.Border = TableBorder.None;
+
+
+        tabela.AddColumn(new TableColumn(board1.GetTable()));
+        tabela.AddColumn(new TableColumn(board2.GetTable()));
+
+        AnsiConsole.Write(tabela);
     }
 }
