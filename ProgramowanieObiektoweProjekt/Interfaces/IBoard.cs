1dd71067b96c16ce15dace4d8f9eb6496428a8c4
Jan <89805433+MortalRave@users.noreply.github.com>
2025-04-21 11:44:28 +0200

    Adding interface 1.00

---
+++ ProgramowanieObiektoweProjekt/Interfaces/IBoard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramowanieObiektoweProjekt.Interfaces
+{
+    internal interface IBoard
+    {
+        // Code...
+    }
+}
