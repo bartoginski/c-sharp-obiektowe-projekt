536a34fe1368013bdd88148f267b2dbdcaeefa24
Jan <89805433+MortalRave@users.noreply.github.com>
2025-04-28 20:32:11 +0200

    Added new interfaces and abstract base ship class.

---
+++ ProgramowanieObiektoweProjekt/Enums/ShotResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramowanieObiektoweProjekt.Enums
+{
+    internal enum ShotResult
+    {
+        Miss,
+        Hit,
+        Sunk
+    }
+}
