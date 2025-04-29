cd9a1d69302cbdc3787e994dd24860789eaa633c
bartoginski <bartoginski@outlook.com>
2025-04-29 20:27:08 +0200

    fix(Iplayer): remove unused code

--- ProgramowanieObiektoweProjekt/Interfaces/IPlayer.cs
+++ ProgramowanieObiektoweProjekt/Interfaces/IPlayer.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ProgramowanieObiektoweProjekt.Models;
-
-namespace ProgramowanieObiektoweProjekt.Interfaces
+﻿namespace ProgramowanieObiektoweProjekt.Interfaces
 {
     internal interface IPlayer
     {
-        string Name { get; }
-        IBoard Board { get; }
-        List<Shot> ShotsFired { get; }
+
     }
 }
