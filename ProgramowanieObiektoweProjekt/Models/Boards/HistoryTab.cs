6a0ea73a03ce039133a9a3974310e72728d6d8bb
Kami Muc <kamil.muc@o2.pl>
2025-05-05 20:41:44 +0200

    Add Layout

--- ProgramowanieObiektoweProjekt/Models/Boards/HistoryTab.cs
+++ ProgramowanieObiektoweProjekt/Models/Boards/HistoryTab.cs
@@ -1,47 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Spectre.Console;
 
 namespace ProgramowanieObiektoweProjekt.Models.Boards
 {
-    /// <summary>
-    /// A class representing the game history, 
-    /// storing information about shots fired, hits and misses.
-    /// </summary>
     internal class HistoryTab
     {
-        /// <summary>
-        /// Number of shots fired.
-        /// </summary>
         public int ShotsFired { get; private set; }
-
-        /// <summary>
-        /// Number of hits.
-        /// </summary>
         public int Hits { get; private set; }
-
-        /// <summary>
-        /// Number of misses (difference between shots fired and hits).
-        /// </summary>
         public int Misses => ShotsFired - Hits;
 
-        /// <summary>
-        /// Registers every shot fired and updates hit and miss statistics.
-        /// </summary>
-        /// <param name="wasHit">A logical value specifying whether the shot was a hit (true) or a miss (false).</param>
         public void RecordShot(bool wasHit)
         {
-            // code...
+            ShotsFired++;
+            if (wasHit)
+                Hits++;
         }
 
-        /// <summary>
-        /// Displays history of shots
-        /// </summary>
         public void DisplayHistoryTab()
         {
-            // code...
+            AnsiConsole.Write(GetHistoryRenderable());
+        }
+
+        public Table GetHistoryRenderable()
+        {
+            var table = new Table()
+                .Title("Historia Strzałów")
+                .AddColumns("Łącznie", "Trafienia", "Pudła");
+
+            table.AddRow(ShotsFired.ToString(), Hits.ToString(), Misses.ToString());
+
+            return table;
         }
     }
 }
+//
