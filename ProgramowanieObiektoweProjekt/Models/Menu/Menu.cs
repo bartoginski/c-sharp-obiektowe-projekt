8840b223e7ddf4e28ae2cc13d92b6650187a4d3d
Jan <89805433+MortalRave@users.noreply.github.com>
2025-05-16 17:52:39 +0200

    Menu prototype

---
+++ ProgramowanieObiektoweProjekt/Models/Menu/Menu.cs
@@ -0,0 +1,48 @@
+﻿using Spectre.Console;
+namespace ProgramowanieObiektoweProjekt.Models.Menu
+{
+    internal class Menu
+    {
+        //1. Start game
+        //2. Games history
+        //3. Autors
+        //4. Exit
+        static public void TitleDisplay()
+        {
+            Console.WriteLine(  "██████╗  █████╗ ████████╗████████╗██╗     ███████╗███████╗██╗  ██╗██╗██████╗\n" +
+                                "██╔══██╗██╔══██╗╚══██╔══╝╚══██╔══╝██║     ██╔════╝██╔════╝██║  ██║██║██╔══██╗\n" +
+                                "██████╔╝███████║   ██║      ██║   ██║     █████╗  ███████╗███████║██║██████╔╝\n" +
+                                "██╔══██╗██╔══██║   ██║      ██║   ██║     ██╔══╝  ╚════██║██╔══██║██║██╔═══╝ \n" +
+                                "██████╔╝██║  ██║   ██║      ██║   ███████╗███████╗███████║██║  ██║██║██║ \n" +
+                                "╚═════╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝     ");
+        }
+
+        static public void MenuDisplay()
+        {
+            var choices = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .PageSize(4)
+                    .AddChoices(new[] {
+                        "Start game", "Games history", "Autors",
+                        "Exit",
+        }));
+
+            if (choices == "Start game")
+            {
+                AnsiConsole.WriteLine("You chosed Start game");
+            }
+            else if (choices == "Games history")
+            {
+                AnsiConsole.WriteLine("You chosed Games history");
+            }
+            else if (choices == "Games history")
+            {
+                AnsiConsole.WriteLine("You chosed Autors");
+            }
+            else
+            {
+                AnsiConsole.WriteLine("You chosed Exit");
+            }
+        }
+    }
+}
