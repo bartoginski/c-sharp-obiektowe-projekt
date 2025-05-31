d2e01982595af43e66e85ef03272f80e3c310039
Albercik26 <bartosznowak26@gmail.com>
2025-05-31 18:24:39 +0200

    BotEasy Update

--- ProgramowanieObiektoweProjekt/Bot/BotEasy.cs
+++ ProgramowanieObiektoweProjekt/Bot/BotEasy.cs
@@ -1,66 +1,134 @@
-using System;
-using System.Collections.Generic;
-using ProgramowanieObiektoweProjekt.Models.Boards; 
-using ProgramowanieObiektoweProjekt.Models.Ships;
-using ProgramowanieObiektoweProjekt.Enums; 
+using ProgramowanieObiektoweProjekt.Enums;
+using ProgramowanieObiektoweProjekt.Models.Boards;
 
-namespace ProgramowanieObiektoweProjekt.Bot.BotEasy
+public class BotEasy : IBot
 {
-    class BotEasy
+    protected const int BoardSize = 10;
+    protected Random _rand = new();
+    protected List<(int x, int y)> _shotsMade = new();
+    protected Queue<(int x, int y)> _huntQueue = new();
+    protected bool _huntingMode = false;
+    protected List<(int x, int y)> _hits = new();
+
+    public virtual string Name => "Easy";
+    public virtual Tuple<int, int> BotShotSelection()
     {
-        public string Name = "Easy";
-
-        public virtual Tuple<int, int> BotShotSelection()
+        if (_huntingMode && _huntQueue.Any())
         {
-            int row, col;
-            bool shotIsNew = false;
-
-            while (!shotIsNew)
+            while (_huntQueue.Any())
             {
-                row = _rand.Next(0, BoardSize);
-                col = _rand.Next(0, BoardSize);
-
-                if (!_shotsMade.Contains((row, col)))
+                var target = _huntQueue.Dequeue();
+                if (!_shotsMade.Contains(target))
                 {
-                    _shotsMade.Add((row, col));
-                    shotIsNew = true;
-                    return Tuple.Create(row, col);
+                    _shotsMade.Add(target); // Add to shotsMade only if it's a new shot
+                    return Tuple.Create(target.x, target.y);
                 }
             }
-            return Tuple.Create(-1, -1);
-        }
-        public virtual void BotShipPlacement(Board board)
-        {
-            List<ShipBase> shipsToPlace = new List<ShipBase>(board.ships);
-
-            foreach (var ship in shipsToPlace)
+            RePopulateHuntQueueFromHits();
+            if (_huntQueue.Any())
             {
-                bool placed = false;
-                while (!placed)
+                var target = _huntQueue.Dequeue();
+                if (!_shotsMade.Contains(target))
                 {
-                    bool isHorizontal = _rand.Next(2) == 0;
-                    Direction orientation = isHorizontal ? Direction.Horizontal : Direction.Vertical;
-
-                    int rowStart, colStart;
-
-                    if (isHorizontal)
-                    {
-                        colStart = _rand.Next(0, BoardSize - ship.Length + 1);
-                        rowStart = _rand.Next(0, BoardSize);
-                    }
-                    else
-                    {
-                        rowStart = _rand.Next(0, BoardSize - ship.Length + 1);
-                        colStart = _rand.Next(0, BoardSize);
-                    }
-
-                    if (board.TryPlaceShip(ship, rowStart, colStart, orientation))
-                    {
-                        placed = true;
-                    }
+                    _shotsMade.Add(target);
+                    return Tuple.Create(target.x, target.y);
                 }
+            }
+            _huntingMode = false;
+        }
 
+        while (true)
+        {
+            int x = _rand.Next(0, BoardSize); // X is column
+            int y = _rand.Next(0, BoardSize); // Y is row
+
+            if (!_shotsMade.Contains((x, y)))
+            {
+                _shotsMade.Add((x, y));
+                return Tuple.Create(x, y);
             }
         }
+    }
+
+    public virtual void BotShipPlacement(Board board)
+    {
+        foreach (var ship in board.ships)
+        {
+            bool placed = false;
+            while (!placed)
+            {
+                Direction dir = _rand.Next(2) == 0 ? Direction.Horizontal : Direction.Vertical;
+                ship.IsHorizontal = (dir == Direction.Horizontal); // Update ship's IsHorizontal property
+
+                int x, y; // x for column, y for row
+
+                if (dir == Direction.Horizontal)
+                {
+                    x = _rand.Next(0, BoardSize - ship.Length + 1);
+                    y = _rand.Next(0, BoardSize); // Y (row) can be anywhere
+                }
+                else // Vertical
+                {
+                    x = _rand.Next(0, BoardSize); // X (column) can be anywhere
+                    y = _rand.Next(0, BoardSize - ship.Length + 1);
+                }
+
+                placed = board.TryPlaceShip(ship, x, y, dir);
+            }
+        }
+    }
+
+    public virtual void InformShotResult(Tuple<int, int> coord, ShotResult result)
+    {
+
+        if (result == ShotResult.Hit)
+        {
+            _huntingMode = true;
+            _hits.Add((coord.Item1, coord.Item2)); // Store the hit coordinate
+            EnqueueAdjacent((coord.Item1, coord.Item2)); // Enqueue adjacent cells to hunt
+        }
+        else if (result == ShotResult.Sunk)
+        {
+            _hits.Clear();
+            _huntQueue.Clear();
+            _huntingMode = false;
+        }
+    }
+
+    protected void EnqueueAdjacent((int x, int y) coord)
+    {
+        int col = coord.x;
+        int row = coord.y;
+
+        var adj = new List<(int x, int y)>
+        {
+            (col, row - 1), // Up
+            (col, row + 1), // Down
+            (col - 1, row), // Left
+            (col + 1, row)  // Right
+        };
+
+        foreach (var t in adj)
+        {
+            if (IsInBounds(t) && !_shotsMade.Contains(t) && !_huntQueue.Contains(t)) // Also check if already in queue to prevent duplicates
+            {
+                _huntQueue.Enqueue(t);
+            }
+        }
+    }
+
+    protected void RePopulateHuntQueueFromHits()
+    {
+        _huntQueue.Clear(); // Clear existing queue before repopulating
+        foreach (var h in _hits)
+        {
+            EnqueueAdjacent(h); // Re-add adjacent cells for all current hits
+        }
+    }
+
+    protected bool IsInBounds((int x, int y) coord)
+    {
+        return coord.x >= 0 && coord.x < BoardSize &&
+               coord.y >= 0 && coord.y < BoardSize;
     }
 }
