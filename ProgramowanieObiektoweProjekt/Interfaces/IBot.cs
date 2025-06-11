519ade002b8684a92b95fd29b3ab615fadb0060c
Bartosz Oginski <bartoginski@outlook.com>
2025-06-11 16:57:41 +0200

    docs(interfaces): add comments

--- ProgramowanieObiektoweProjekt/Interfaces/IBot.cs
+++ ProgramowanieObiektoweProjekt/Interfaces/IBot.cs
@@ -5,7 +5,7 @@ internal interface IBot
 {
     string Name { get; }
     Tuple<int, int> BotShotSelection();
-    void BotShipPlacement(Board board); // Board to konkretna klasa, nie interfejs IBoard
+    void BotShipPlacement(Board board); // Board is concrete class, not IBoard interface
     void InformShotResult(Tuple<int, int> shotCoordinates, ShotResult result);
-    void AddCellsToAvoid(List<(int col, int row)> cells); // <-- DODAJ TĘ LINIĘ
+    void AddCellsToAvoid(List<(int col, int row)> cells); // Add cells to avoid shooting
 }
